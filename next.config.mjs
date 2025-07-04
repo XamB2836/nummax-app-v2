337626d21d7452bfcd7f4162e9601a59b23f42f2
XamB2836 <xamb3606@gmail.com>
2025-07-04 13:46:14 -0400

    test vercel !

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false,    // <-- désactive la minification SWC
+
 };
 
-module.exports = nextConfig;
+export default nextConfig;
