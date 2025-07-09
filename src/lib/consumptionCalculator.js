2b1be4a4810df61fbd7415bdef94464232652a14
XamB2836 <xamb3606@gmail.com>
2025-07-09 15:54:15 -0400

    Refactor consumption calculation to round result to the nearest whole number

--- src/lib/consumptionCalculator.js
+++ src/lib/consumptionCalculator.js
@@ -2,5 +2,6 @@
 
 export function calculateConsumption(screenWidthMM, screenHeightMM, wattPerM2) {
   const screenAreaM2 = (screenWidthMM / 1000) * (screenHeightMM / 1000)
-  return screenAreaM2 * wattPerM2
+  const consumption = screenAreaM2 * wattPerM2
+  return Math.round(consumption)  // arrondi à l’unité la plus proche
 }
